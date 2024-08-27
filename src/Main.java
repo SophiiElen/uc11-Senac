e26bd80872c4fc9d9f8f1bd6a977585dc74562ab
SophiiElen <sophiaelenbarbosa@gmail.com>
2024-08-26 22:38:33 -0300

    criação main class + cadastroVIEW definida como tela principal

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author sophi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        cadastroVIEW tela = new cadastroVIEW();
+        tela.setLocationRelativeTo(null);
+        tela.setVisible(true);
+    }
+
+}
